22636fe6efbc5f18114d5e58aa1c1bcf481e5d38
Zadkiel Cerrada <mr.cerrada.zadkiel@gmail.com>
2022-07-28 21:23:44 -0700

    Add function factorial + comments

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+// Square
 function squared(num) {
     return num * num;
 }
 
+// Cube
 function cubed(num) {
     return num * num * num;
+}
+
+// Factorial
+function factorial(num) {
+    if (num < 0) return undefined;
+    if (num === 0) return 1;
+    let x = num - 1;
+    while (x > 1) {
+        num *= x;
+        x--;
+    }
+    return num;
 }
